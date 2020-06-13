565be354869c0c284c926c262a1537d385fc4af9
kdteck <54762152+kdteck@users.noreply.github.com>
2020-06-14 02:19:27 +0530

    Update instatool.sh

--- instatool.sh
+++ instatool.sh
@@ -12,13 +12,13 @@ echo
 echo
 echo
 echo $i"["$bi"•"$i"]"$me"──────────────────────────────────"$i"["$bi"•"$i"]"
-echo $i" |"$cy"      PLEASE SUBSCRIBE MR HACKER  "$i"             |"
+echo $i" |"$cy"      PLEASE SUBSCRIBE dhaneshkumar  "$i"             |"
 echo $i" |"$me"        INSTAGRAM HACKING TOOLS               "$i" |"
 echo $i" |"$me"=========================================="$i"|"
-echo $i" |"$pu" AUTHOR :"$ku"   KHOKAN DAS [MR HACKER]"$i"        |"
+echo $i" |"$pu" AUTHOR :"$ku"   KHOKAN DAS [dhanesh]"$i"        |"
 echo $i" |"$pu" FACEBOOK :"$ku"  KHOKAN DAS"$i"                   |"
 echo $i" |"$pu" INSTAGRAM  :"$cy"Impish_khoka"$i"                 |"
-echo $i" |"$pu" Contact  WhatsApp :"$ku" 918906791455"$i"         |"
+echo $i" |"$pu" Contact  WhatsApp :"$ku" 917708955776"$i"         |"
 echo $i" |"$pu" Contact Gmail :"$ku"    khoka.kd@gmail.com"$i"    |"
 echo $i"["$bi"•"$i"]"$me"──────────────────────────────────"$i"["$bi"•"$i"]"
 echo
@@ -37,7 +37,7 @@ echo $i"────────────────────────
 echo $i"|"$me" X"$i" |"$cy"     EXIT    PROGRAM      "$i"|"$i"    |"$cy"   "$i" |"
 echo $i"────────────────────────────────    ─────"
 echo
-echo $me"┌==="$bi"["$i"MR HACKER"$bi"]"$me"======"$bi"["$i""Support me""$bi"]"
+echo $me"┌==="$bi"["$i"DHANESH"$bi"]"$me"======"$bi"["$i""Support me""$bi"]"
 echo $me"¦"
 read -p"└──# " pil
 
@@ -142,9 +142,9 @@ figlet -f slant "E X I T"|lolcat
 sleep 2
 echo $cy"THANKS FOR USEING"
 sleep 2
-echo $i"join Telegram :- MrHacker for New script "
+echo $i"join Telegram :- dhanesh for New script "
 sleep 2
-echo $ku"WhatsApp :"$i" 918906791455"
+echo $ku"WhatsApp :"$i" 917708955776"
 echo $ku"Instagram :"$i" Impish_khoka"
 sleep 2
 echo $pur">> Thanks  &  Support me <<"
